65ba18460c9ed40a2464955d7d375a407db24235
bzab076 <b.zabukovec@auckland.ac.nz>
2014-08-29 09:37:14 +1200

    Refactored init method to accept arbitrary replacement strings.

--- src/main/java/nz/ac/auckland/common/testrunner/BatheTestUtils.java
+++ src/main/java/nz/ac/auckland/common/testrunner/BatheTestUtils.java
@@ -2,27 +2,52 @@ package nz.ac.auckland.common.testrunner;
 
 import bathe.BatheInitializerProcessor;
 
+import java.util.Enumeration;
+import java.util.Properties;
+
 /**
  * @author: Richard Vowles - https://plus.google.com/+RichardVowles
  */
 public class BatheTestUtils {
+    /*
+    * Use props for all replacement strings.
+     */
+    public static void init(Class<?> klass, Properties props) {
+
+        BatheCommandLine stp = klass.getAnnotation(BatheCommandLine.class);
+
+        String[] args;
+
+        if (stp != null) {
+            args = new String[stp.value().length];
+            int count = 0;
+
+            for(String arg : stp.value()) {
+
+                if(props!=null) {
+                    for (Enumeration<String> e = (Enumeration<String>)props.propertyNames(); e.hasMoreElements();) {
+
+                        String propertyName = e.nextElement();
+                        arg = arg.replace(propertyName,props.getProperty(propertyName));
+                    }
+                }
+
+                args[count++] = arg;
+            }
+        } else {
+            args = new String[] {};
+        }
+
+        new BatheInitializerProcessor().process(args, "", klass.getClassLoader());
+
+    }
+
 	public static void init(Class<?> klass) {
-		BatheCommandLine stp = klass.getAnnotation(BatheCommandLine.class);
 
-		String []args;
+        String userHome = System.getProperty("user.home");
+        Properties homeOnlyProperty = new Properties();
+        homeOnlyProperty.setProperty("$home",userHome);
 
-		if (stp != null) {
-			args = new String[stp.value().length];
-			String userHome = System.getProperty("user.home");
-			int count = 0;
-
-			for(String arg : stp.value()) {
-				args[count++] = arg.replace("$home", userHome);
-			}
-		} else {
-			args = new String[] {};
-		}
-
-		new BatheInitializerProcessor().process(args, "", klass.getClassLoader());
+        init(klass, homeOnlyProperty);
 	}
 }
